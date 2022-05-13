cd4479722228121e31ec6c9a6a22213dbae355ac
Mikalai Karamach <mikalai_karamach@epam.com>
2022-05-13 16:22:45 +0300

    Added "Autocode greeting" task

---
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -0,0 +1,7 @@
+package com.mjc.stage0;
+
+public class FirstApplication {
+    public static void main(String[] args) {
+        // Write your code here!
+    }
+}
---
+++ src/test/java/com/mjc/stage0/FirstApplicationTest.java
@@ -0,0 +1,41 @@
+package com.mjc.stage0;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.nio.file.Files.readAllLines;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FirstApplicationTest {
+    private static Stream<?> getFileLines(Path sourcePath) {
+        try {
+            return readAllLines(sourcePath, StandardCharsets.UTF_8).stream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Stream.empty();
+    }
+
+    @Test
+    public void dummyTest() throws IOException {
+        String result = Files.walk(Paths.get("src/main/java/com/mjc/stage0"))
+                .filter(Files::isRegularFile)
+                .filter(p -> p.toString().endsWith(".java"))
+                .flatMap(FirstApplicationTest::getFileLines)
+                .map(Object::toString)
+                .collect(Collectors.joining("\n"));
+
+        List<String> requiredElements = List.of("System.out.println", "Hello World!");
+        requiredElements.forEach(el ->
+                assertTrue(result.contains(el), String.format("'%s' should be used", el))
+        );
+    }
+}
