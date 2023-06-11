583d6209bc64d52bbde1f320cdf2e6d707c235db
Muzaffar55 <97522350+Muzaffar55@users.noreply.github.com>
2023-06-11 21:27:42 +0500

    Update FirstApplication.java
    First commit

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -1,6 +1,6 @@
 package com.mjc.stage0;
 
-public class FirstApplication {
+public class FirstApplication { 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
