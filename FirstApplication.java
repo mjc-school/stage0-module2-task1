9eefc09a600eb46a1ff3f5d57e264447620cff49
g.dalalishvili <121316474+braumi@users.noreply.github.com>
2023-10-31 21:26:35 +0400

    FirstApplication.java

--- FirstApplication.java
+++ FirstApplication.java
@@ -1,6 +1,7 @@
-
-public class Main {
-    public static void main(String[] args){
+public class FirstApplication {
+    public static void FirstApplication(String[] args)
+    {
         System.out.println("Hello, World!");
+
     }
 }
